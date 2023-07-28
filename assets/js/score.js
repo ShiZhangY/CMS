d71f92b1dcbca65569b3976e4e78de66cf3002ef
雍士章 <941077522@qq.com>
2023-07-28 20:26:50 +0800

    成绩双击事件和失焦事件

--- assets/js/score.js
+++ assets/js/score.js
@@ -20,5 +20,27 @@ async function renderScoreData() {
     tbody.innerHTML = str
 }
 renderScoreData()
-
 //双击表格进入编辑状态
+tbody.addEventListener('dblclick', function (e) {
+    //获取表格原本的值
+    if (e.target.classList.contains('score')) {
+        let oldData = e.target.innerText
+        const input = document.createElement('input')
+        input.type = 'text'
+        input.value = oldData
+        input.setAttribute('data-id', `${e.target.parentNode.children[0].innerText}`)
+        e.target.appendChild(input)
+        input.style.display = 'block'
+        document.querySelector('input').focus()
+    }
+})
+
+//失去焦点事件
+tbody.addEventListener('blur', function (e) {
+    if (e.target.tagName === 'INPUT') {
+        console.log(e.target.value);
+        console.log(e.target.dataset.id);
+
+        e.target.parentNode.removeChild(e.target);
+    }
+}, true)
